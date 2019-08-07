ff56b6a8457c87594a1be3193041c0121aca2e62
Erislândio Soares <erislandio.soares@acct.global>
2019-08-06 23:22:21 -0300

    add deslike controllers

---
+++ backend/src/controllers/deslikeControllers.js
@@ -0,0 +1,31 @@
+const Dev = require("../models/Dev");
+
+module.exports = {
+    async store(req, res) {
+        try {
+            const { user } = req.headers;
+            const { id } = req.params;
+
+            const userLogged = await Dev.findById(user);
+            const userFound = await Dev.findById(id);
+
+            console.log(id, user);
+
+            if (!user) {
+                return res.status(400).send({
+                    message: "User not found!"
+                });
+            }
+
+            userLogged.deslikes.push(userFound._id);
+
+            await userLogged.save();
+
+            return res.status(200).send(userLogged);
+        } catch (error) {
+            return res.status(500).send({
+                error
+            });
+        }
+    }
+};
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,9 +2,11 @@ const express = require("express");
 const routes = express.Router();
 const devController = require("./controllers/DevControllers");
 const likeController = require("./controllers/LikeController");
+const desLikeController = require("./controllers/deslikeControllers");
 
 routes.post("/dev", devController.store);
 routes.get("/dev", devController.index);
 routes.post("/dev/:id/like", likeController.store);
+routes.post("/dev/:id/deslike", desLikeController.store);
 
 module.exports = routes;
