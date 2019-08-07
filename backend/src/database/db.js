ebd764291c69770a25262e6024bedaa499732c1f
Erislândio Soares <erislandio.soares@acct.global>
2019-08-06 22:19:35 -0300

    add conecction database mlab

---
+++ backend/src/database/db.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const mongodbUri = `mongodb://@ds127843.mlab.com:27843/omnistack`;
+const config = require("../config.json");
+
+mongoose.connect(mongodbUri, {
+    useNewUrlParser: true,
+    config
+});
+const conn = mongoose.connection;
+conn.on("error", console.error.bind(console, "Erro!:"));
+
+conn.once("open", () => {
+    console.log("conectado a database Mlab");
+});
+
+mongoose.Promise = global.Promise;
+
+module.exports = mongoose;
--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const server = express();
 const routes = require("./routes");
 const PORT = 3000;
+const mongoose = require("./database/db");
 
 server.use(express.json());
 server.use(routes);
